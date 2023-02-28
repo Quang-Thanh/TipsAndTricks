ee82ed7d806d9ed853548cefdccbdc21d1be9791
Quang-Thanh <quangthanhvu711@gmail.com>
2023-02-28 23:04:50 +0700

    Định nghĩa các lớp thực thể

---
+++ TatBlog.Core/Entities/Author.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Contracts;
+
+namespace TatBlog.Core.Entities
+{
+    public class Author : IEntity
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string UrlSlug { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DateTime JoineDate { get; set; }
+
+        public string Email { get; set; }
+
+        public string Notes { get; set; }
+
+        //Danh sách các bài viết của tác giả
+        public IList<Post> Posts { get; set; }
+    }
+}
---
+++ TatBlog.Core/Entities/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Contracts;
+
+namespace TatBlog.Core.Entities
+{
+    public class Category : IEntity
+    {
+        //mã chuyên mục
+        public int Id { get; set; }
+
+        //tên chuyên mục, chủ đề
+        public string Name { get; set; }
+
+        //Tên định dạng để tạo URL
+        public string UrlSlug { get; set; }
+
+        //mô tả chuyên mục
+        public string Description { get; set; }
+
+        //Đánh giá chuyên mục được hiện thị trên menu
+        public bool ShowOnMenu { get; set; }
+
+        //Danh sách các bài viết thuộc chuyên mục
+        public IList<Post> Posts { get; set; }
+    }
+}
