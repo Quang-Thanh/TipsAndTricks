236929dc1e29d1d97ce57c25a6b12fb1bdbddfaa
Quang-Thanh <quangthanhvu711@gmail.com>
2023-03-05 22:11:17 +0700

    code

--- TatBlog.WebApp/Program.cs
+++ TatBlog.WebApp/Program.cs
@@ -1,6 +1,44 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
+{
+	//Thêm các dịch vụ đucợ yên cầu bới MVC Framework
+	builder.Services.AddControllersWithViews();
+}
 var app = builder.Build();
+{
+	//Cấu hình HTTP Request pipeline
 
-app.MapGet("/", () => "Hello World!");
+	// Thêm middleware để hiện thị thông báo lỗi
+	if(app.Environment.IsDevelopment())
+	{
+		app.UseDeveloperExceptionPage();
+	}	
+	else
+	{
+		app.UseExceptionHandler("/Blog/Eror");
+
+		//Thêm middleware cho việc áp dụng HSTS (thêm header
+		// Strict-Transport-Security vào HTTP Respose).
+		app.UseHsts();
+	}	
+
+	//Thêm middleware để chuyển hướng HTTP sáng HTTPS
+	app.UseHttpsRedirection();
+
+	//Thêm middleware phục vụ các yêu cầu liên quan
+	// tới các tin nội dung tĩnh như hình ảnh, css,...
+	app.UseStaticFiles();
+
+	//Thêm middleware lựa chọn endpoint phù hợp nhất
+	//để xử lý một HTTP request.
+	app.UseRouting();
+
+	//Định nghĩa router template, route constraint cho các
+	//endpoints kết hợp với các action trong các controller
+	app.MapControllerRoute(
+		name:"default", 
+		pattern:"{controller=Blog}/{acion=Index}/{id?}");
+}
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
