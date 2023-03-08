031ba08c004e96c6a2b57f24b668f964cbfdef7a
Quang-Thanh <quangthanhvu711@gmail.com>
2023-03-08 23:29:24 +0700

    Hiển thị danh sách chủ đề

---
+++ TatBlog.WebApp/Components/CategoriesWidget.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TatBlog.Services.Blogs;
+
+namespace TatBlog.WebApp.Components
+{
+	public class CategoriesWidget : ViewComponent
+	{
+		private readonly IBlogRepository _blogRepositry;
+
+		public CategoriesWidget(IBlogRepository blogRepositry)
+		{
+			_blogRepositry = blogRepositry;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			//Lấy danh sách chủ đề
+			var categories = await _blogRepositry.GetCategoriesAsync();
+
+			return View(categories);
+		}
+	}
+}
