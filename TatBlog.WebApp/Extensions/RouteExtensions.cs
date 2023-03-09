9180122dd0b26dd4a052213c654b5ddffdf212a4
Quang-Thanh <quangthanhvu711@gmail.com>
2023-03-09 18:57:31 +0700

    tinh giản code 2

--- TatBlog.WebApp/Extensions/RouteExtensions.cs
+++ TatBlog.WebApp/Extensions/RouteExtensions.cs
@@ -5,32 +5,34 @@ using TatBlog.Services.Blogs;
 
 namespace TatBlog.WebApp.Extensions
 {
-	public class RouteExtensions
+	public static class RouteExtensions
 	{
-		//Thêm các dịch vụ được yêu cầu bỏi MVC
-		public static WebApplicationBuilder ConfigureMvc(
-			this WebApplicationBuilder builder) 
+		//Định nghĩa route template, route constraint cho các
+		//endpoint kết hợp với cá action trong các controller
+
+		public static IEndpointRouteBuilder UseBlogRoutes(
+			this IEndpointRouteBuilder endpoinrs)
 		{
-			builder.Services.AddControllersWithViews();
-			builder.Services.AddResponseCompression();
+			endpoinrs.MapControllerRoute(
+				name: "post-by-category",
+				pattern: "blog/category/{slug}",
+				defaults: new { controller = "Blog", action = "Category" });
 
-			return builder;
-		}
+			endpoinrs.MapControllerRoute(
+				name: "post-by-tag",
+				pattern: "blog/tag/{slug}",
+				defaults: new { controller = "Blog", action = "Tag" });
 
-		//Đăng kí các dịch vụ với DI Container
-		public static WebApplicationBuilder ConfigureServices(
-			this WebApplicationBuilder builder) 
-		{
-			builder.Services.AddDbContext<BlogDbContext>(options =>
-				options.UseSqlServer(
-					builder.Configuration
-					.GetConnectionString("DefaultConnection")));
+			endpoinrs.MapControllerRoute(
+				name: "single-post",
+				pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
+				defaults: new { controller = "Blog", action = "Post" });
 
-			builder.Services.AddScoped<IBlogRepository,  BlogRepository>();
-			builder.Services.AddScoped<IDataSeeder, DataSeeder>();
+			endpoinrs.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Blog}/{action=Index}/{id?}");
 
-			return builder;
-		
+			return endpoinrs;
 		}
 	}
 }
--- TatBlog.WebApp/Program.cs
+++ TatBlog.WebApp/Program.cs
@@ -4,84 +4,23 @@ using TatBlog.Core.Entities;
 using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeders;
 using TatBlog.Services.Blogs;
+using TatBlog.WebApp.Extensions;
+
 
 var builder = WebApplication.CreateBuilder(args);
 {
-	//Thêm các dịch vụ đucợ yên cầu bới MVC Framework
-	builder.Services.AddControllersWithViews();
-
-	//Đăng ký các dịch vụ với DI Container
-	builder.Services.AddDbContext<BlogDbContext>(options =>
-		options.UseSqlServer(
-			builder.Configuration.GetConnectionString("DefaultConnection")));
-	builder.Services.AddScoped<IBlogRepository, BlogRepository>();
-	builder.Services.AddScoped<IDataSeeder, DataSeeder>();
+	builder
+		.ConfigureMvc()
+		.ConfigureServices();
 }
+
 var app = builder.Build();
 {
-	//Cấu hình HTTP Request pipeline
-
-	// Thêm middleware để hiện thị thông báo lỗi
-	if(app.Environment.IsDevelopment())
-	{
-		app.UseDeveloperExceptionPage();
-	}	
-	else
-	{
-		app.UseExceptionHandler("/Blog/Eror");
-
-		//Thêm middleware cho việc áp dụng HSTS (thêm header
-		// Strict-Transport-Security vào HTTP Respose).
-		app.UseHsts();
-	}	
-
-	//Thêm middleware để chuyển hướng HTTP sáng HTTPS
-	app.UseHttpsRedirection();
-
-	//Thêm middleware phục vụ các yêu cầu liên quan
-	// tới các tin nội dung tĩnh như hình ảnh, css,...
-	app.UseStaticFiles();
-
-	//Thêm middleware lựa chọn endpoint phù hợp nhất
-	//để xử lý một HTTP request.
-	app.UseRouting();
-
-	//Định nghĩa router template, route constraint cho các
-	//endpoints kết hợp với các action trong các controller
-
-
-	app.MapControllerRoute(
-			//name: "default",
-			//pattern: "{controller=Blog}/{action=Index}/{id?}"
-			name: "post-by-category",
-			pattern: "blog/category/{slug}",
-			defaults: new { controller = "Blog", action = "Category"});
-	
-	app.MapControllerRoute(
-		name: "post-by-tag",
-		pattern: "blog/tag/{slug}",
-		defaults: new { controller = "Blog", action = "Tag"});
-
-	app.MapControllerRoute(
-		name: "single",
-		pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
-		defaults:new { controller = "Blog", action = "Post"});
-
-	app.MapControllerRoute(
-		name: "default",
-		pattern: "{controller=Blog}/{action=Index}/{id?}");
-	//app.UseEndpoints(endpoints =>
-	//{
-	//	endpoints.MapControllerRoute(
-	//		name: "default",
-	//		pattern: "{controller=Blog}/{action=Index}/{id?}");
-	//});
-}
-
-using (var scope = app.Services.CreateScope())
-{
-	var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
-	seeder.Initialize();
+	app.UseRequestPipeline();
+	app.UseBlogRoutes();
+	app.UseDataSeeder();
 }
 
 app.Run();
+
+
