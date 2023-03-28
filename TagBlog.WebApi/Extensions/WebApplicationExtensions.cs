7cbc1ebb55221d4ded594a5a2a4269521772f253
Quang-Thanh <quangthanhvu711@gmail.com>
2023-03-28 23:48:21 +0700

    B3 Cau hinh cac dich vu su dung trong du an webapi

---
+++ TagBlog.WebApi/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TatBlog.Data.Contexts;
+using TatBlog.Services.Blogs;
+using TatBlog.Services.Media;
+using TatBlog.Services.Timing;
+
+namespace TatBlog.WebApi.Extensions
+{
+	public static class WebApplicationExtensions
+	{
+
+		public static WebApplicationBuilder ConfigureServices(
+			this WebApplicationBuilder builder)
+		{
+			builder.Services.AddMemoryCache();
+
+			builder.Services.AddDbContext<BlogDbContext>(option => 
+			option.UseSqlServer(
+				builder.Configuration
+				.GetConnectionString("DefaultConnection")));
+
+			builder.Services
+				.AddScoped<ITimeProvider, ITimeProvider>();
+			builder.Services
+				.AddScoped<IMediaManager, LocalFileSystemMediaManager>();
+			builder.Services
+				.AddScoped<IBlogRepository, BlogRepository>();
+			builder.Services
+				.AddScoped<IAuthorRepository, IAuthorRepository>();
+			return builder;
+		}
+
+		public static WebApplicationBuilder ConfigureCors(this WebApplicationBuilder builder)
+		{
+			builder.Services.AddCors(options =>
+			{
+				options.AddPolicy("TatBlogApp", policyBuilder => policyBuilder
+							)
+			}
+		}
+
+	}
+}
--- TagBlog.WebApi/Program.cs
+++ TagBlog.WebApi/Program.cs
@@ -1,5 +1,21 @@
+using TatBlog.WebApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+{
+	//add services to the container.
+	builder
+		.ConfigureCors()
+		.ConfigureNLog()
+		.ConfigureServices()
+		.ConfigureSwaggerOpenApi();
+}
 
 var app = builder.Build();
+{
+	//Configure the HTTP request pipeline
+	app.SetupRequestPipeline();
+
+	app.Run();
+}
 
 app.Run();
