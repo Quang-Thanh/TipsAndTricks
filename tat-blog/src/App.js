5da23129a92721e8679ad85e06bbacbf875f22e8
Quang-Thanh <quangthanhvu711@gmail.com>
2023-04-04 16:29:09 +0700

    update folder

---
+++ tat-blog/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Sidebar from './Components/Slidebar';
+import Footer from './Components/Footer';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+function App(){
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-9'>
+
+
+            </div>
+            <div className='col-3 border-start'>
+              <Sidebar/>
+            </div>
+          </div>
+        </div>
+        <Footer/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
