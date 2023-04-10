846f8a6bea89c7e179b840018258aa74b17d30dd
Quang-Thanh <quangthanhvu711@gmail.com>
2023-04-10 21:59:54 +0700

    fix loi lab5 keyword

--- tat-blog/src/Components/Pager.js
+++ tat-blog/src/Components/Pager.js
@@ -3,53 +3,56 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import Button from "react-bootstrap/Button";
 
-const Pager = ({ postQuery, metadata }) => {
-  console.log(postQuery);
+const Pager = ({ postquery, metadata }) => {
+  console.log(">>> check postQuery: ", postquery);
+  console.log(">>> check metadata: ", metadata);
 
-  let pageCount = metadata.pageCount,
+  let pageCount = metadata.pagedCount,
     hasNextPage = metadata.hasNextPage,
     hasPreviousPage = metadata.hasPreviousPage,
     pageNumber = metadata.pageNumber,
     pageSize = metadata.pageSize,
-    actionName = '', slug = '',
-    keyword = postQuery.keyword ?? '';
+    actionName = "",
+    slug = "",
+    keyword = postquery.keyword ?? "";
 
   if (pageCount > 1) {
     return (
-      <div className='text-center my-4'>
-        {hasPreviousPage
-          ? <Link
-            to = {`/blog/${actionName}?slug=${slug}&k=${keyword}
+      <div className="text-center my-4">
+        {hasPreviousPage ? (
+          <Link
+            to={`/blog/${actionName}?slug=${slug}&k=${keyword}
             &p=${pageNumber - 1}&ps=${pageSize}`}
-            className='btn btn-info'>
-              <FontAwesomeIcon icon={faArrowLeft} />
-              &nbsp;Trang trước
-            </Link>
-            : <Button variant='outline-secondary' disabled>
-              <FontAwesomeIcon icon={faArrowLeft} />
-              &nbsp;Trang trước
-            </Button>
-        }
-        {hasNextPage
-          ? <Link
-            to = {`/blog/${actionName}?slug=${slug}&k=${keyword}
+            className="btn btn-info"
+          >
+            <FontAwesomeIcon icon={faArrowLeft} />
+            &nbsp;Trang trước
+          </Link>
+        ) : (
+          <Button variant="outline-secondary" disabled>
+            <FontAwesomeIcon icon={faArrowLeft} />
+            &nbsp;Trang trước
+          </Button>
+        )}
+        {hasNextPage ? (
+          <Link
+            to={`/blog/${actionName}?slug=${slug}&k=${keyword}
             &p=${pageNumber + 1}&ps=${pageSize}`}
-            className='btn btn-info ms-1'>
-              Trang sau&nbsp;
-              <FontAwesomeIcon icon={faArrowRight} />
-            </Link>
-            : <button variant='outline-secondary' className='ms-1' disabled>
-              Trang sau&nbsp;
-              <FontAwesomeIcon icon={faArrowRight} />
-              </button>
-        }
+            className="btn btn-info ms-1"
+          >
+            Trang sau&nbsp;
+            <FontAwesomeIcon icon={faArrowRight} />
+          </Link>
+        ) : (
+          <button variant="outline-secondary" className="ms-1" disabled>
+            Trang sau&nbsp;
+            <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+        )}
       </div>
     );
   }
-  return (
-    <Link>
-    </Link>
-  );
-}
+  return <Link></Link>;
+};
 
 export default Pager;
--- tat-blog/src/Pages/index.js
+++ tat-blog/src/Pages/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import PostItem from '../Components/PostItem';
+import PostItem from "../Components/PostItem";
 import Pager from "../Components/Pager";
 import { getPosts } from "../Services/BlogRepository";
 
@@ -14,42 +14,44 @@ const Index = () => {
   }
 
   let query = useQuery(),
-    k = query.get('k') ?? '',
-    p = query.get('p') ?? 1,
-    ps = query.get('ps') ?? 10;
+    keyword = query.get("k") ?? "",
+    p = query.get("p") ?? 1,
+    ps = query.get("ps") ?? 5;
 
   useEffect(() => {
-    document.title = 'Trang chủ';
+    document.title = "Trang chủ";
 
-    getPosts(k, ps, p).then(data => {
-      if (data){
+    getPosts(keyword, ps, p).then((data) => {
+      if (data) {
         setPostList(data.items);
-        setMetadata(data.metadata);}
-      else
-        setPostList([]);
-    })
-  }, [k, ps, p]);
+        setMetadata(data.metadata);
+      } else setPostList([]);
+    });
+  }, [keyword, ps, p]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [postList]);
 
-  if (postList.length > 0)
+  if (postList.length > 0) {
     return (
-      <div className='p-4'>
+      <div className="p-4">
         {postList.map((item, index) => {
-          return (
-            <PostItem postItem={item} key={index} />
-          );
+          return <PostItem postItem={item} key={index} />;
         })}
 
-        <Pager postquery={{ 'keyword' : k }} metadata={metadata} />
         {/* <Pager postquery={{ 'keyword' : k }} metadata={metadata} /> */}
+        <Pager postquery={{ keyword }} metadata={metadata} />
       </div>
     );
-  else return (
-    <></>
-  );
-}
+  } else {
+    // syntax nhu nay nha
+    return <></>;
+  }
+
+  // else return (
+  //   <></>
+  // );
+};
 
 export default Index;
